c93e37a726e718ecd2f906acdd3415a591d1ef11
kunalsonwane77 <kunalsonwane77@gmail.com>
2024-07-30 21:29:16 +0530

    largest finding of arry

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const students = ["kunal","rahul","sejal","piyush","yash"]
 for(const school of students){
         console.log(`hello ${school}`)
 }
+ 
+// finding largest number from the given Array
+const marks = [88,74,44,124,541]
+
+let larg = marks[0]
+for(let i=1; i<marks.length;i++)
+{
+    if(marks[i]>larg){
+        larg=marks[i]
+    }
+   
+}
+console.log(larg)
